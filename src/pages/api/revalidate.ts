86093beb88f8bd249799ed447db7cf9f115b227a
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2023-07-11 22:44:33 +0500

    Revelidate API

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -20,8 +20,6 @@ export default async function handler(req: NextApiRequest,res: NextApiResponse) 
         return res.json({revalidated:true})
     }
     } catch (err) {
-      // If there was an error, Next.js will continue
-      // to show the last successfully generated page
       console.log(err)
       return res.status(500).send('Error revalidating');
     }

1d75d4e1f0508403eabf6fe4278e37768b87e1f0
Haseeb <haseebullahmemon33@gmail.com>
2023-07-20 22:38:13 +0500

    api issue

---
+++ src/app-modules/repositories/api-repository/api-respository.ts
@@ -0,0 +1,29 @@
+import { apiHandler } from '../../connection/api-client';
+
+interface ApiProps {
+  token?: string;
+  data?: any;
+}
+
+export const loginApi = (props: ApiProps): Promise<any> => {
+  const {  data } = props;
+  return apiHandler().post('api/user/login', data);
+};
+
+export const RegisterApi = (props: ApiProps): Promise<any> => {
+    const { token, data } = props;
+    return apiHandler(token).post('/api/retister', data);
+};
+
+
+
+export const getVerifyApi = (props: ApiProps): Promise<any> => {
+    const { token, data } = props;
+    return apiHandler(token).post('/api/verify', data);
+};
+
+export const GetAllUserApi = (props: ApiProps): Promise<any> => {
+    const { token, data } = props;
+    return apiHandler(token).post('/api/getuser', data);
+};
+
---
+++ src/components/global/navbar/style.tsx
@@ -0,0 +1,5 @@
+const style = () => {
+  return <div></div>;
+};
+
+export default style;

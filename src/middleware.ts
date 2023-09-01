20c7a927438e528dbe3a0069e5f03cbbcf642151
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2023-09-01 18:54:03 +0500

    Strapi admin api integrated & code cleaning

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,11 @@
-// export { default } from "next-auth/middleware";
 import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
+
 
 export default withAuth(
-  // `withAuth` augments your `Request` with the user's token.
-  function middleware(req) {
+  function middleware() {
 
-    // if (req.nextUrl.pathname.startsWith("/profile") && !req.nextauth.token )
-    //   return NextResponse.rewrite(
-    //     new URL("/auth/login?message=You Are Not Authorized!", req.url)
-    //   );
   },
-  // {
 
-  // }
 );
 
 export const config = {

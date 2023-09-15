9a7987035e618ecf15c6febde4c75671af491bb4
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2023-09-15 21:07:54 +0500

    faqs added woth serapi

--- src/components/global/page-constructor/page-constructor.tsx
+++ src/components/global/page-constructor/page-constructor.tsx
@@ -1,37 +1,119 @@
+'use client';
 import Banner from '@/components/banner';
 import FAQS from '@/components/faqs';
 import Footer from '@/components/global/footer';
 import NavigationBar from '@/components/global/navbar';
+import parser, { HTMLReactParserOptions, domToReact } from 'html-react-parser';
 import { NextPage } from 'next';
-import React from 'react';
+import Image from 'next/image';
 import { PageData } from 'types';
+import logo from '../../../../public/wrap.jpg';
 import Seo from '../seo';
+// const PageConstructor: NextPage<{ data: PageData }> = ({ data }) => {
+//   const transform = (node) => {
 
+//     if (node.attribs && node.attribs.class) {
+//       node.attribs.className = node.attribs.class;
+//       delete node.attribs.class;
+//     }
+//     console.log(node);
+//     return node;
+//   };
+
+//   const options: HTMLReactParserOptions = {
+//     replace: transform
+//   }
+
+//   return (
+//     <>
+//       <Seo title={data?.Title} />
+//       {data?.nav?.data && (
+//         <div className="header">
+//           <NavigationBar data={[]} />
+//         </div>
+//       )}
+
+//       <Banner />
+//       <section className="m-3 bg-neutral-900 dark:bg-gray-800 dark:text-gray-100">
+//         <div className="container p-4 ">
+//           <div className="grid grid-cols-1 gap-4 bg-neutral-900 md:grid-cols-12">
+//             {data.columns.map((column, index) => (
+//               <div
+//                 key={index}
+//                 className={`col-span-${column.grid} rounded-lg border p-4 shadow-md`}
+//               >
+//                 {parser(column.editor,options)} {/* Parse HTML with options */}
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//       </section>
+//       <FAQS />
+//       {data?.footer?.data && (
+//         <div className="footer bottom-0 w-full">
+//           <Footer />
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+// export default PageConstructor;
 const PageConstructor: NextPage<{ data: PageData }> = ({ data }) => {
-  
+  const options: HTMLReactParserOptions = {
+    replace: ({ attribs, children, name }) => {
+      console.log(name);
+      if (!attribs) {
+        return;
+      }
+      if (attribs?.id === 'main') {
+        return (
+          <h1 style={{ fontSize: 42 }}>{domToReact(children, options)}</h1>
+        );
+      }
+      if (name === 'summary') {
+        return (
+          <summary className="cursor-pointer text-orange-500">
+            {domToReact(children, options)}
+          </summary>
+        );
+      }
+      if (name === 'img') {
+        return <Image width={50} height={50} src={logo} alt="" />;
+      }
+      if (attribs?.classname === 'mce-accordion') {
+        return (
+          <details className="mce-accordion bg-white p-4 shadow-md transition duration-300 hover:shadow-lg">
+            {domToReact(children, options)}
+          </details>
+        );
+      }
+    },
+  };
   return (
     <>
-    <Seo title={data.Title}/>
+      <Seo title={data?.Title} />
       {data?.nav?.data && (
         <div className="header">
           <NavigationBar data={[]} />
         </div>
       )}
-     
-      <Banner/>
-      <div className="container p-4">
-        <div className="grid grid-cols-1 md:grid-cols-12 gap-4">
-          {data.columns.map((column, index) => (
-            <div
-              key={index}
-              className={`col-span-${column.grid} p-4 border rounded-lg shadow-md`}
-            >
-              <p>{column.editor}</p>
-            </div>
-          ))}
+
+      <Banner />
+      <section className="m-2 bg-neutral-900 dark:bg-gray-800 dark:text-gray-100">
+        <div className="container p-4 ">
+          <div className="grid grid-cols-1 gap-4 bg-neutral-900 md:grid-cols-12">
+            {data.columns.map((column, index) => (
+              <div
+                key={index}
+                className={`col-span-${column.grid} p-2 shadow-md`}
+              >
+                {parser(column.editor, options)}
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      <FAQS/>
+      </section>
       {data?.footer?.data && (
         <div className="footer bottom-0 w-full">
           <Footer />
@@ -42,3 +124,84 @@ const PageConstructor: NextPage<{ data: PageData }> = ({ data }) => {
 };
 
 export default PageConstructor;
+// 'use client';
+// import Banner from '@/components/banner';
+// import FAQS from '@/components/faqs';
+// import Footer from '@/components/global/footer';
+// import NavigationBar from '@/components/global/navbar';
+// import { NextPage } from 'next';
+// import { PageData } from 'types';
+// import Seo from '../seo';
+// import logo  from "../../../public/wrap.jpg"
+// import parse, { domToReact } from 'html-react-parser';
+// import { HTMLReactParserOptions, Element } from 'html-react-parser';
+// const PageConstructor: NextPage<{ data: PageData }> = ({ data }) => {
+// const html = `
+// <details className="mce-accordion">
+//               <summary className="cursor-pointer">
+//                 Accordion summary...
+//               </summary>
+//               <div className="mt-2 text-black">Accordion body...</div>
+//             </details>
+// `;
+
+// const options:HTMLReactParserOptions = {
+//   replace: ({attribs, children,name}) => {
+//     console.log(name)
+//       if (!attribs) {
+//         return;
+//       }
+
+//       if (attribs?.id === 'main') {
+//         return <h1 style={{ fontSize: 42 }}>{domToReact(children, options)}</h1>;
+//       }
+//       if (name === 'summary') {
+//         return <summary className="cursor-pointer text-orange-500">{domToReact(children, options)}</summary>;
+//       }
+//       if (attribs?.classname === 'mce-accordion') {
+
+//         return (
+//           <details className="mce-accordion bg-white p-4 shadow-md transition duration-300 hover:shadow-lg">
+//             {domToReact(children, options)}
+//           </details>
+//         );
+//       }
+//     }
+//   }
+
+//   return (
+//     <>
+//       {/* <Seo title={data?.Title} />
+//       {data?.nav?.data && (
+//         <div className="header">
+//           <NavigationBar data={[]} />
+//         </div>
+//       )}
+
+//       <Banner />
+//       <section className="dark:bg-gray-800 dark:text-gray-100 bg-neutral-900 m-3">
+//       <div className="container p-4 ">
+//         <div className="grid grid-cols-1 gap-4 md:grid-cols-12 bg-neutral-900">
+//           {data.columns.map((column, index) => (
+//             <div
+//               key={index}
+//               className={`col-span-${column.grid} rounded-lg border p-4 shadow-md`}
+//             >
+//              {parser(column.editor)}
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//       </section>
+//       <FAQS />
+//       {data?.footer?.data && (
+//         <div className="footer bottom-0 w-full">
+//           <Footer />
+//         </div>
+//       )} */}
+//       {parse(html, options)}
+//     </>
+//   );
+// };
+
+// export default PageConstructor;
